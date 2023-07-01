870ad38a81b175933961ae595d02f8cdc2b74e3d
museeskinder <museeskinder@users.noreply.github.com>
2023-07-01 08:41:34 +0300

    error words and modified word population

--- Hangman/hangman.js
+++ Hangman/hangman.js
@@ -10,8 +10,7 @@ const correct = document.querySelector('.container .main .correct')
 const errorWords = document.getElementById('error-words');
 const won = document.querySelector('.message-won');
 const lost = document.querySelector('.message-word');
-const pLetters = document.querySelectorAll('.correct .word');
-console.log(pLetters);
+const populatedWords = correct.childNodes;
 
 let words = ["message", "programming", "anthropology",
              "hacker", "javascript", "java", "world", "google", "code", "html"];
@@ -35,13 +34,39 @@ const prepareWord = (letter) => {
     wordDiv.appendChild(wordP);
     wordDiv.appendChild(lineDiv);
 
-    wordP.className = `letter ${letter}`;
+    wordP.id = `${letter}`;
+    wordP.className = 'letter';
     lineDiv.className = 'line';
     return wordDiv;
 }
 
+const setErrorWord = (letter) => {
+    let temp = errorWords.innerText;
+    if(temp === '')
+        errorWords.innerText = letter;
+    else {
+        temp += `, ${letter}`;
+        errorWords.innerText = temp;
+    }
+}
+
 let word = getWord(prev);
+console.log(word);
 
 [...word].forEach((e) => {
     correct.appendChild(prepareWord(e));
+})
+
+document.addEventListener('keypress', e => {
+    let collection = [...populatedWords].filter((f) => 
+        f.firstChild.id === e.key && !f.firstChild.className.includes('set')
+    )
+    if(collection.length !== 0) {
+        collection.forEach((g) => {
+            g.firstChild.innerText = e.key;
+        })
+    }
+    else {
+        setErrorWord(e.key);
+    }
 })
