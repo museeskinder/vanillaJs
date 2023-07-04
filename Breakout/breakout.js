59b3a9e73238259953d1b0ed9e646d41239b484e
museeskinder <museeskinder@users.noreply.github.com>
2023-07-04 20:01:17 +0300

    canvas ball

--- Breakout/breakout.js
+++ Breakout/breakout.js
@@ -1,6 +1,31 @@
+//Rules elements
 const rulesBtn = document.getElementById('rules-btn');
 const closeBtn = document.getElementById('close-btn');
 const rules = document.getElementById('rules');
 
+//canvas
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d')
+
+//Rules event handlers
 rulesBtn.addEventListener('click', e => rules.classList.add('show'));
 closeBtn.addEventListener('click', e => rules.classList.remove('show'));
+
+//ball prop
+const ball = {
+    x : canvas.width / 2,
+    y : canvas.height / 2,
+    size : 10,
+    dx : 4,
+    dy : -4
+}
+
+//drawing ball
+const drawBall = () => {
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+}
+drawBall();
