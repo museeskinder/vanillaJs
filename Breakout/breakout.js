89d10a1f3c5b569eef60e6536a9c7027cda864ec
museeskinder <museeskinder@users.noreply.github.com>
2023-07-04 20:18:32 +0300

    canvas: paddle and score

--- Breakout/breakout.js
+++ Breakout/breakout.js
@@ -11,7 +11,9 @@ const ctx = canvas.getContext('2d')
 rulesBtn.addEventListener('click', e => rules.classList.add('show'));
 closeBtn.addEventListener('click', e => rules.classList.remove('show'));
 
-//ball prop
+let score = 0;
+
+//props
 const ball = {
     x : canvas.width / 2,
     y : canvas.height / 2,
@@ -19,8 +21,15 @@ const ball = {
     dx : 4,
     dy : -4
 }
+const paddle = {
+    x : canvas.width / 2 - 40,
+    y : canvas.height -20,
+    width : 90,
+    height: 10,
+    dx : 0
+}
 
-//drawing ball
+//drawing functions
 const drawBall = () => {
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ball.size, 0, Math.PI * 2);
@@ -28,4 +37,18 @@ const drawBall = () => {
     ctx.fill();
     ctx.closePath();
 }
+const drawPaddle = () => {
+    ctx.beginPath();
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+    ctx.fillStyle = '#0095dd';
+    ctx.fill();
+    ctx.closePath();
+}
+const drawScore = () => { 
+    ctx.fillStyle = 'black';
+    ctx.font = '20px Arial';
+    ctx.fillText(`Score: ${score}`, canvas.width - 100, 30);
+}
 drawBall();
+drawPaddle();
+drawScore();
